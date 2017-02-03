d93a6e0cd7590c966efb5c81626be74c0a775db9
lakshit anand <lakshit1001@gmail.com>
2017-02-03 18:04:21 +0530

    initial code by rohan

---
+++ include/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'bookfoxi_cmp');
+define('DB_PASSWORD', 'campus@2017');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'bookfoxi_campusbox');
+?>
