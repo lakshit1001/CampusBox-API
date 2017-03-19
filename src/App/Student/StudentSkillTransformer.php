38d9794da66c1110641fbc855ba6c2bb36d76015
Lakshit1001 <Lakshit1001@ymail.com>
2017-03-19 16:49:17 +0530

    student skills only send name now

--- src/App/Student/StudentSkillTransformer.php
+++ src/App/Student/StudentSkillTransformer.php
@@ -13,9 +13,7 @@ class StudentSkillTransformer extends Fractal\TransformerAbstract
     public function transform(StudentSkill $skill)
     {
         return [
-            "id" => (integer)$skill->id?: 0 ,
-            "username" => (string)$skill->username?: null,
-            "skill_name" => (string)$skill->skill_name?: null 
+            "name" => (string)$skill->skill_name?: null 
         ];
     }
 }
