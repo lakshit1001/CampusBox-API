c32abfbbcc882c027c01a3c4230caddb02887a7b
Ayush <ayushpahwa96@gmail.com>
2017-05-28 12:43:33 +0530

    Cleaned output for content items according to type

--- src/App/Content/ContentItemsTransformer.php
+++ src/App/Content/ContentItemsTransformer.php
@@ -10,18 +10,30 @@ use League\Fractal;
 class ContentItemsTransformer extends Fractal\TransformerAbstract {
 
 	public function transform(ContentItems $content_items) {
-		return [
-			// "event_bookmark_id" => (integer) $content_items->event_bookmark_id ?: 0,
-		"id" => (integer) $content_items->content_item_id ?: 0,
-		"priority" => (integer) $content_items->priority ?: 0,
-		"type" => (string) $content_items->content_item_type ?: 4,
-		"description" => (string) $content_items->description ?: null,
-		"image" => (string) $content_items->image ?: null,
-		"embed" => [
-			"html" => (string) $content_items->embed ?: null,
-			"url" => (string) $content_items->embed_url ?: 0,
-			],                
-		];
-		
-	}
+			if ($content_items->content_item_type == 'text') {
+				return [
+					"id" => (integer) $content_items->content_item_id ?: 0,
+					"type" => (string) $content_items->content_item_type ?: 4,
+					"priority" => (string) $content_items->priority ?: 0,
+					"description" => (string) $content_items->description ?: null	
+				];
+			} elseif ($content_items->content_item_type == 'image' || $content_items->$content_item_type == 'cover') {
+				return [
+					"id" => (integer) $content_items->content_item_id ?: 0,
+					"type" => (string) $content_items->content_item_type ?: 4,
+					"priority" => (string) $content_items->priority ?: 0,
+					"image" => (string) $content_items->image ?: null	
+				];
+			} else{
+				return [
+					"id" => (integer) $content_items->content_item_id ?: 0,
+					"type" => (string) $content_items->content_item_type ?: 4,
+					"priority" => (string) $content_items->priority ?: 0,
+					"embed" => [
+					"html" => (string) $content_items->embed ?: null,
+					"url" => (string) $content_items->embed_url ?: 0,
+					]
+				];
+			}
+		}  // End of ContentsItemsTransformer
 }
